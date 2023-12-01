798a310bb209d25a0285f288e8018cc4736c56dd
komugi8 <8448eito@gmail.com>
2023-12-01 15:29:56 +0900

    不要なコメントの削除

--- cmd/main.go
+++ cmd/main.go
@@ -12,20 +12,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// func handleRoutes() {
-
-// 	taskHandler := handler.NewTaskHandler()
-
-// 	e := echo.New()
-// 	task := e.Group("/tasks")
-// 	task.GET("", taskHandler.GetTasks)
-// 	task.POST("", taskHandler.CreateTask)
-// 	task.GET("/:id", taskHandler.GetTask)
-// 	task.PUT("/:id", taskHandler.UpdateTask)
-// 	task.DELETE("/:id", taskHandler.DeleteTask)
-
-// 	e.Logger.Fatal(e.Start(":8080"))
-
 func main() {
 	cfg := config.New()
 	db, err := database.NewDB(cfg)
